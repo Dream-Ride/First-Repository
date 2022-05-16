cbe1996dae51b081e98d4a577265f305cc4eaeb1
Dream-Ride <103274522+Dream-Ride@users.noreply.github.com>
2022-05-15 23:51:28 -0300

    Update wordle_2.py

--- wordle_2.py
+++ wordle_2.py
@@ -1,15 +1,8 @@
 if __name__ == "__main__":
+   
     print (" Filtrando palabras de 5 letras ")
-    diccionario = open('diccionarios.txt', 'r')
-    lista_palabras = []
-    for linea in diccionario:
-        linea = diccionario.readline()
-        #if letras en palabra = 5 => agregar a lista
-        if len(linea) == 6:
-            lista_palabras.append(linea.strip)
-            
-        if linea == 1290:
-            break
-    #palabras = cantidad de palabras en lista_palabras
-    print(" Operación comppletada con exito ")
-    print(" Se encontraron " str(len(lista_palabras)) " palabras de 5 letras ")
+    with open ('diccionarios.txt', 'r') as diccionario:
+        listaPalabras = [str(linea) for linea in diccionario if len(linea) == 6]
+
+    print(" Operación completada con exito ")
+    print(" Se encontraron ", str(len(listaPalabras)), " palabras de 5 letras ")
