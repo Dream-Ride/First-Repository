7a9c665accf68382bdd753c9c38f855d384ea883
Dream-Ride <103274522+Dream-Ride@users.noreply.github.com>
2022-06-03 14:06:25 -0300

    Update wordle_2.py

--- wordle_2.py
+++ wordle_2.py
@@ -1,6 +1,34 @@
 from colorama import init, Fore
 import random
 
+def evaluar(entrada):
+    while entrada not in str(listaPalabras):
+        if len(entrada) == 5 and entrada.isalpha() and entrada not in str(listaPalabras):
+            print(Fore.RED + " ERROR: La palabra no es valida " )
+            entrada = input(Fore.WHITE + " Introduce otra palabra: " )
+        elif len(entrada) == 5 and not entrada.isalpha():
+            print (Fore.RED + " ERROR: La palabra contiene caracteres no validos " )
+            entrada = input(Fore.WHITE + " Introduce otra palabra: " )
+        elif len(entrada) != 5 and entrada.isalpha():
+            print (Fore.RED + " ERROR: la palabra no contiene 5 letras " )
+            entrada = input(Fore.WHITE + " Introduce otra palabra: " )
+        elif len(entrada) != 5 and not entrada.isalpha():
+            print(Fore.RED + " ERROR: La palabra no contiene 5 letras ni caracteres validos " )
+            entrada = input(Fore.WHITE + " Introduce otra palabra: " )
+        else:
+            break
+
+def analizar(entrada):
+    for letra in entrada and numer_letra in range (0,4):
+        if letra in palabra:
+            if letra == palabra[numer_letra]:
+                print(Fore.GREEN + letra, end= "")
+            else:
+                print(Fore.YELLOW + letra, end= "")
+            if letra not in palabra:
+                print(Fore.RED + letra, end= "")
+            break
+
 if __name__ == "__main__":
     init()
 
@@ -15,41 +43,22 @@ if __name__ == "__main__":
         print(palabra)
 
         entrada = input( " Escribe una palabra de 5 letras: " ).casefold()
+        n_entrada = 0
+        evaluar(entrada)
 
-        while entrada != palabra:
-            while entrada not in str(listaPalabras):
-                if len(entrada) == 5 and entrada.isalpha() and entrada not in str(listaPalabras):
-                    print(Fore.RED + " ERROR: La palabra no es valida " )
-                    entrada = input(Fore.WHITE + " Introduce otra palabra: " )
-                elif len(entrada) == 5 and not entrada.isalpha():
-                    print (Fore.RED + " ERROR: La palabra contiene caracteres no validos " )
-                    entrada = input(Fore.WHITE + " Introduce otra palabra: " )
-                elif len(entrada) != 5 and entrada.isalpha():
-                    print (Fore.RED + " ERROR: la palabra no contiene 5 letras " )
-                    entrada = input(Fore.WHITE + " Introduce otra palabra: " )
-                elif len(entrada) != 5 and not entrada.isalpha():
-                    print(Fore.RED + " ERROR: La palabra no contiene 5 letras ni caracteres validos " )
-                    entrada = input(Fore.WHITE + " Introduce otra palabra: " )
-                print(Fore.GREEN + " La palabra es valida " )
-        
-            numer_letra = 0
-        
-            for letra in entrada:
-                if letra in palabra:
-                    if letra == palabra[numer_letra]:
-                        print(Fore.GREEN + letra, end= "")
-                    else:
-                        print(Fore.YELLOW + letra, end= "")
-                else:
-                    print(Fore.RED + letra, end= "")
-                numer_letra += 1
-                if numer_letra == 5:
-                    break
-
+        while n_entrada < 5:
             if entrada == palabra:
+                    print (Fore.GREEN + palabra)
+                    print (Fore.WHITE + " La palabra es valida \n Palabara encontrada " )
+                    break
+            numer_letra = 0
+            numer_letra += 1
+            if numer_letra == 5:
+                entrada = input( " Introduce otra palabra " )
+                evaluar(entrada)
+                n_entrada += 1
                 break
-            else:
-                entrada = input (Fore.WHITE + " \n Introduce otra palabra: " )
+            if n_entrada == 5 and entrada != palabra:
+                print(Fore.RED + " Perdiste " )
 
-        print ( " La palabra es valida \n Palabara encontrada " )  
             
